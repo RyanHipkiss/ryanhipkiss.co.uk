2507bf1bf3be3b7391144a02db8c9cd055d4f95d
Ryan Hipkiss <ryan@ryanhipkiss.co.uk>
2024-10-01 16:54:28 +0100

    add github action

---
+++ scripts.js
@@ -0,0 +1,10 @@
+// Wait for the window to load before triggering the animation
+window.onload = function() {
+    const name = document.getElementById('name');
+    const subhead = document.getElementById('subhead');
+    name.classList.add('animate'); // Add the animate class to trigger the animation
+    
+    name.addEventListener('transitionend', () => {
+        subhead.classList.add('fade-in')
+    })
+};
