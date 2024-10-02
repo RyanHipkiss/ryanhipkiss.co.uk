77098dca3a8bd136955159cc6da6be84fd6b7d8e
Ryan Hipkiss <ryan@ryanhipkiss.co.uk>
2024-10-02 19:17:30 +0100

    who am i section

--- scripts.js
+++ scripts.js
@@ -3,7 +3,7 @@ window.onload = function() {
     const name = document.getElementById('name');
     const subhead = document.getElementById('subhead');
     name.classList.add('animate'); // Add the animate class to trigger the animation
-    
+
     name.addEventListener('transitionend', () => {
         subhead.classList.add('fade-in')
     })
